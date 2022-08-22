9aa8dedf928d88bc269f6f37cd2f4ad8eeec45ac
s317ansar <78231169+s317ansar@users.noreply.github.com>
2022-08-22 22:37:00 +0530

    Frist

---
+++ ansarspring/src/test/java/com/example/ansarspring/AnsarspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ansarspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsarspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
